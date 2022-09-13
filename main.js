6671a2572ef07006e1e0ef207cce18d52458b42b
Stiven Herrera <stiven3722@gmail.com>
2022-09-13 00:22:03 -0500

    Tercera Actualizacion

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ let pasajeros = document.getElementById("pasajeros").value;
 let resultado = document.getElementById("capaMostrar");
 
 function liquidar() {
+  resultado.innerHTML = "Valor del tiquete";
+  //Barranquilla
   if (ciudad == 0 && silla == 0) {
     let adicional = 0;
     let tiquetes = 150000 * pasajeros;
@@ -19,6 +21,282 @@ function liquidar() {
       `<br>` +
       "PesoAdicional: " +
       adicional +
+      `<br>` +
+      "Total a pagar: " +
+      tp);
+  }
+  if (ciudad == 0 && silla == 1) {
+    let adicional = 0;
+    let tiquetes = 150000 * pasajeros;
+    if (pesoMaletas > 50) {
+      adicional = (pesoMaletas - 50) * 15000;
+    }
+    tp = adicional + tiquetes + 20000;
+    return (resultado.innerHTML =
+      "Tiquetes: " +
+      tiquetes +
+      `<br>` +
+      "PesoAdicional: " +
+      adicional +
+      `<br>` +
+      "Silla Ejecutiva: " +
+      20000 +
+      `<br>` +
+      "Total a pagar: " +
+      tp);
+  }
+  if (ciudad == 0 && silla == 2) {
+    let adicional = 0;
+    let tiquetes = 150000 * pasajeros;
+    if (pesoMaletas > 50) {
+      adicional = (pesoMaletas - 50) * 15000;
+    }
+    tp = adicional + tiquetes + 40000;
+    return (resultado.innerHTML =
+      "Tiquetes: " +
+      tiquetes +
+      `<br>` +
+      "PesoAdicional: " +
+      adicional +
+      `<br>` +
+      "Silla VIP (Reclama tu copa de whisky): " +
+      40000 +
+      `<br>` +
+      "Total a pagar: " +
+      tp);
+  }
+
+  //Medellin
+  if (ciudad == 1 && silla == 0) {
+    let adicional = 0;
+    let tiquetes = 100000 * pasajeros;
+    if (pesoMaletas > 50) {
+      adicional = (pesoMaletas - 50) * 15000;
+    }
+    tp = adicional + tiquetes;
+    return (resultado.innerHTML =
+      "Tiquetes: " +
+      tiquetes +
+      `<br>` +
+      "PesoAdicional: " +
+      adicional +
+      `<br>` +
+      "Total a pagar: " +
+      tp);
+  }
+  if (ciudad == 1 && silla == 1) {
+    let adicional = 0;
+    let tiquetes = 100000 * pasajeros;
+    if (pesoMaletas > 50) {
+      adicional = (pesoMaletas - 50) * 15000;
+    }
+    tp = adicional + tiquetes + 20000;
+    return (resultado.innerHTML =
+      "Tiquetes: " +
+      tiquetes +
+      `<br>` +
+      "PesoAdicional: " +
+      adicional +
+      `<br>` +
+      "Silla Ejecutiva: " +
+      20000 +
+      `<br>` +
+      "Total a pagar: " +
+      tp);
+  }
+  if (ciudad == 1 && silla == 2) {
+    let adicional = 0;
+    let tiquetes = 100000 * pasajeros;
+    if (pesoMaletas > 50) {
+      adicional = (pesoMaletas - 50) * 15000;
+    }
+    tp = adicional + tiquetes + 40000;
+    return (resultado.innerHTML =
+      "Tiquetes: " +
+      tiquetes +
+      `<br>` +
+      "PesoAdicional: " +
+      adicional +
+      `<br>` +
+      "Silla VIP (Reclama tu copa de whisky): " +
+      40000 +
+      `<br>` +
+      "Total a pagar: " +
+      tp);
+  }
+
+  //Cali
+  if (ciudad == 2 && silla == 0) {
+    let adicional = 0;
+    let tiquetes = 90000 * pasajeros;
+    if (pesoMaletas > 50) {
+      adicional = (pesoMaletas - 50) * 15000;
+    }
+    tp = adicional + tiquetes;
+    return (resultado.innerHTML =
+      "Tiquetes: " +
+      tiquetes +
+      `<br>` +
+      "PesoAdicional: " +
+      adicional +
+      `<br>` +
+      "Total a pagar: " +
+      tp);
+  }
+  if (ciudad == 2 && silla == 1) {
+    let adicional = 0;
+    let tiquetes = 90000 * pasajeros;
+    if (pesoMaletas > 50) {
+      adicional = (pesoMaletas - 50) * 15000;
+    }
+    tp = adicional + tiquetes + 20000;
+    return (resultado.innerHTML =
+      "Tiquetes: " +
+      tiquetes +
+      `<br>` +
+      "PesoAdicional: " +
+      adicional +
+      `<br>` +
+      "Silla Ejecutiva: " +
+      20000 +
+      `<br>` +
+      "Total a pagar: " +
+      tp);
+  }
+  if (ciudad == 2 && silla == 2) {
+    let adicional = 0;
+    let tiquetes = 90000 * pasajeros;
+    if (pesoMaletas > 50) {
+      adicional = (pesoMaletas - 50) * 15000;
+    }
+    tp = adicional + tiquetes + 40000;
+    return (resultado.innerHTML =
+      "Tiquetes: " +
+      tiquetes +
+      `<br>` +
+      "PesoAdicional: " +
+      adicional +
+      `<br>` +
+      "Silla VIP (Reclama tu copa de whisky): " +
+      40000 +
+      `<br>` +
+      "Total a pagar: " +
+      tp);
+  }
+
+  //Cartagena
+  if (ciudad == 3 && silla == 0) {
+    let adicional = 0;
+    let tiquetes = 170000 * pasajeros;
+    if (pesoMaletas > 50) {
+      adicional = (pesoMaletas - 50) * 15000;
+    }
+    tp = adicional + tiquetes;
+    return (resultado.innerHTML =
+      "Tiquetes: " +
+      tiquetes +
+      `<br>` +
+      "PesoAdicional: " +
+      adicional +
+      `<br>` +
+      "Total a pagar: " +
+      tp);
+  }
+  if (ciudad == 3 && silla == 1) {
+    let adicional = 0;
+    let tiquetes = 170000 * pasajeros;
+    if (pesoMaletas > 50) {
+      adicional = (pesoMaletas - 50) * 15000;
+    }
+    tp = adicional + tiquetes + 20000;
+    return (resultado.innerHTML =
+      "Tiquetes: " +
+      tiquetes +
+      `<br>` +
+      "PesoAdicional: " +
+      adicional +
+      `<br>` +
+      "Silla Ejecutiva: " +
+      20000 +
+      `<br>` +
+      "Total a pagar: " +
+      tp);
+  }
+  if (ciudad == 3 && silla == 2) {
+    let adicional = 0;
+    let tiquetes = 170000 * pasajeros;
+    if (pesoMaletas > 50) {
+      adicional = (pesoMaletas - 50) * 15000;
+    }
+    tp = adicional + tiquetes + 40000;
+    return (resultado.innerHTML =
+      "Tiquetes: " +
+      tiquetes +
+      `<br>` +
+      "PesoAdicional: " +
+      adicional +
+      `<br>` +
+      "Silla VIP (Reclama tu copa de whisky): " +
+      40000 +
+      `<br>` +
+      "Total a pagar: " +
+      tp);
+  }
+
+  //Bogota
+  if (ciudad == 4 && silla == 0) {
+    let adicional = 0;
+    let tiquetes = 120000 * pasajeros;
+    if (pesoMaletas > 50) {
+      adicional = (pesoMaletas - 50) * 15000;
+    }
+    tp = adicional + tiquetes;
+    return (resultado.innerHTML =
+      "Tiquetes: " +
+      tiquetes +
+      `<br>` +
+      "PesoAdicional: " +
+      adicional +
+      `<br>` +
+      "Total a pagar: " +
+      tp);
+  }
+  if (ciudad == 4 && silla == 1) {
+    let adicional = 0;
+    let tiquetes = 120000 * pasajeros;
+    if (pesoMaletas > 50) {
+      adicional = (pesoMaletas - 50) * 15000;
+    }
+    tp = adicional + tiquetes + 20000;
+    return (resultado.innerHTML =
+      "Tiquetes: " +
+      tiquetes +
+      `<br>` +
+      "PesoAdicional: " +
+      adicional +
+      `<br>` +
+      "Silla Ejecutiva: " +
+      20000 +
+      `<br>` +
+      "Total a pagar: " +
+      tp);
+  }
+  if (ciudad == 4 && silla == 2) {
+    let adicional = 0;
+    let tiquetes = 120000 * pasajeros;
+    if (pesoMaletas > 50) {
+      adicional = (pesoMaletas - 50) * 15000;
+    }
+    tp = adicional + tiquetes + 40000;
+    return (resultado.innerHTML =
+      "Tiquetes: " +
+      tiquetes +
+      `<br>` +
+      "PesoAdicional: " +
+      adicional +
+      `<br>` +
+      "Silla VIP (Reclama tu copa de whisky): " +
+      40000 +
       `<br>` +
       "Total a pagar: " +
       tp);
